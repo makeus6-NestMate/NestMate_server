5082b7c63cee4299c19bdcddfb9dcc7077f781bf
diydriller <dhrhd080@naver.com>
2021-03-15 23:48:33 +0900

    commit

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('./config/express');
 const {logger} = require('./config/winston');
 
 //const port = 3000;
-express().listen(process.env.PORT);
+express().listen(process.env.PORT||4000);
 //logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
