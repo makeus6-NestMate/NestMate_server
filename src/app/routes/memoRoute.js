d76a3d4daceda63fd7b4bea575349fb50e213997
diydriller <dhrhd080@naver.com>
2021-03-20 19:25:37 +0900

    commit

--- src/app/routes/memoRoute.js
+++ src/app/routes/memoRoute.js
@@ -2,6 +2,6 @@ module.exports = function(app){
     const memo = require('../controllers/memoController');
     const {verify} = require('../../../config/middlewares');
 
-    app.post('/room/:roomId/memo',memo.createMemo);
+    //app.post('/room/:roomId/memo',memo.createMemo);
     
 };
